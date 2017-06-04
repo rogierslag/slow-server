a192a7d763c79d2869bae5441f9768b07cb2223d
Rogier Slag <rogier.slag@gmail.com>
2017-06-04 11:19:23 +0200

    Monitor inflight requests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var port = process.env.PORT || 8080;
 
 var app = express();
+var inflight = 0;
 
 app.enable('trust proxy');
 app.use('/favicon.ico', function(req, res) {
@@ -11,10 +12,12 @@ app.use('/favicon.ico', function(req, res) {
 app.use('/*', function (req, res) {
 	var timeout = Number(req.query.response_time) || 1;
 	var status = Number(req.query.return_status) || 200;
+	inflight++;
 	setTimeout(function() {
 		res.status(status).end('Service responded with status code ' + status + ', delayed by ' + timeout + ' milliseconds. Use the query parameter `response_time` to give the timeout in milliseconds, or specify the returned status code using `return_status`.');
+		console.log(JSON.stringify({host: req.headers.host, datetime: (new Date()).toISOString(), client: req.ip, response_delay: timeout, response_status: status, inflight_requests: inflight}));
+		inflight--;
 	}, timeout);
-	console.log(JSON.stringify({host: req.headers.host, datetime: (new Date()).toISOString(), client: req.ip, response_delay: timeout, response_status: status}));
 });
 app.listen(port, function() {
 	console.log('Slow server started at port ' + port);
