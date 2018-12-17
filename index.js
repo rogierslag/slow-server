751bdf767db8d25d64a4b523211d958e5afc024e
Rogier Slag <rogier.slag@gmail.com>
2018-12-17 15:52:23 +0100

    Hadle 204

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 
 var port = process.env.PORT || 8080;
 
@@ -6,6 +7,7 @@ var app = express();
 var inflight = 0;
 
 app.enable('trust proxy');
+app.use(cors());
 app.use('/favicon.ico', function(req, res) {
 	res.status(404).end();
 });
@@ -18,7 +20,11 @@ app.use('/*', function (req, res) {
 	var status = Number(req.query.return_status) || 200;
 	inflight++;
 	setTimeout(function() {
-		res.status(status).end('Service responded with status code ' + status + ', delayed by ' + timeout + ' milliseconds. Use the query parameter `response_time` to give the timeout in milliseconds, or specify the returned status code using `return_status`. Add the query param `log_headers` to log the headers of the incoming request.');
+		if (status == 204) {
+			res.status(status).end();
+		} else {
+			res.status(status).end('Service responded with status code ' + status + ', delayed by ' + timeout + ' milliseconds. Use the query parameter `response_time` to give the timeout in milliseconds, or specify the returned status code using `return_status`. Add the query param `log_headers` to log the headers of the incoming request.');
+		}
 		console.log(JSON.stringify({host: req.headers.host, datetime: (new Date()).toISOString(), client: req.ip, response_delay: timeout, response_status: status, inflight_requests: inflight}));
 		inflight--;
 	}, timeout);
